ed09a461148e148ded5b37de8a88e24a95011b9a
GregorBerger <gregorberger1997@gmail.com>
2020-11-18 18:31:26 +0100

    Map import

--- src/main/webapp/ftl/main.ftl
+++ src/main/webapp/ftl/main.ftl
@@ -1,12 +1,63 @@
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/html">
-<head>
-    <title>Prva stran</title>
-    <script src="https://d3js.org/d3.v5.js"></script>
-    <script src="./static/js/main.js"></script>
+<html lang="en">
+<script src="https://d3js.org/d3.v4.min.js"></script>
+<script src="//d3js.org/topojson.v1.min.js"></script>
+<style>
 
-</head>
+    path {
+        fill: #ccc;
+        stroke: #fff;
+        stroke-width: .5px;
+    }
+
+    circle {
+        fill: #fff;
+        fill-opacity: 0.4;
+        stroke: #111;
+    }
+
+    path:hover {
+        fill: pink;
+    }
+    circle.active {
+        fill: blue;
+    }
+
+
+</style>
 <body>
-    <h1>Hello ${ime}</h1>
-</body>
-</html>
+
+
+<script>
+
+    var width = 960;
+    var height = 620;
+
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width",width)
+        .attr("height",height);
+
+    var projection = d3.geoIdentity().reflectY(true);
+
+    var path = d3.geoPath(projection);
+
+    d3.json("./static/SR.geojson", function(err, geojson) {
+
+        projection.fitSize([width,height],geojson);
+
+        // svg.append("d").selectAll("path").data(geojson.features)
+        //     .enter().append("path").attr("d", path);
+
+        svg.append("g")
+            .attr("class", "black")
+            .selectAll("path")
+            .data(geojson.features)
+            .enter()
+            .append("path")
+            .attr("d", path);
+
+    })
+
+</script>
+<script src="https://d3js.org/d3.v5.js"></script>
+<script src="./static/js/main.js"></script>
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,41 +3,48 @@
 d3.json('https://api.sledilnik.org/api/municipalities')
     .then(function(data) {
         let todayData = data[data.length - 1];
-        let regionsData = todayData.regions;
-        var regionsMap = new Map();
+        mapTodaysData(todayData, todayData.month);
 
-        const regionsDataMap = new Map(Object.entries(regionsData));
-        for (var mapElement of regionsDataMap) {
-            var regionName = mapElement[0];
-            var obcineInRegion = mapElement[1];
-
-            var activeCases = 0;
-            var confirmedToDate = 0;
-            var deceasedToDate = 0;
-            regionsMap[regionName] = obcineInRegion;
-
-            const obcinaDataMap = new Map(Object.entries(obcineInRegion));
-            for (var obcinaElement of obcinaDataMap) {
-                var singleEl = obcinaElement[1];
-                if (singleEl.activeCases != null) {
-                    activeCases += singleEl.activeCases;
-                }
-
-                if (singleEl.confirmedToDate != null) {
-                    confirmedToDate += singleEl.confirmedToDate;
-                }
-
-                if (singleEl.deceasedToDate != null) {
-                    deceasedToDate += singleEl.deceasedToDate;
-                }
-            }
-            var regionData = {};
-            regionData.activeCases = activeCases;
-            regionData.confirmedToDate = confirmedToDate;
-            regionData.deceasedToDate = deceasedToDate;
-
-            regionsMap[regionName] = regionData;
-        }
-
-        console.log(regionsMap);
     });
+
+function mapTodaysData(todayData, month) {
+    let regionsData = todayData.regions;
+    var regionsMap = new Map();
+
+    const regionsDataMap = new Map(Object.entries(regionsData));
+    for (var mapElement of regionsDataMap) {
+        var regionName = mapElement[0];
+        var obcineInRegion = mapElement[1];
+
+        var activeCases = 0;
+        var confirmedToDate = 0;
+        var deceasedToDate = 0;
+        regionsMap[regionName] = obcineInRegion;
+
+        const obcinaDataMap = new Map(Object.entries(obcineInRegion));
+        for (var obcinaElement of obcinaDataMap) {
+            var singleEl = obcinaElement[1];
+            if (singleEl.activeCases != null) {
+                activeCases += singleEl.activeCases;
+            }
+
+            if (singleEl.confirmedToDate != null) {
+                confirmedToDate += singleEl.confirmedToDate;
+            }
+
+            if (singleEl.deceasedToDate != null) {
+                deceasedToDate += singleEl.deceasedToDate;
+            }
+        }
+        var regionData = {};
+        regionData.activeCases = activeCases;
+        regionData.confirmedToDate = confirmedToDate;
+        regionData.deceasedToDate = deceasedToDate;
+        regionData.month = month;
+
+        regionsMap[regionName] = regionData;
+    }
+
+    console.log(regionsMap);
+    return regionsMap;
+}
